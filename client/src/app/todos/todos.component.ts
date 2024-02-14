35ab1534cc65716f69b09e3d6b365e35b51ff8e4
Simon <simonhm31@gmail.com>
2024-02-13 22:47:56 -0600

    Changed todos.component.html, 'NgFor' seems to have connected some thing together, todo owner list is now displayed in the HTML

--- client/src/app/todos/todos.component.ts
+++ client/src/app/todos/todos.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { TodoService } from './todo.service';
+import { Todo } from './todo';
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit{
+  todos: Todo[];
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit() {
+    this.todoService.getTodos().subscribe((todos) => {
+      this.todos = todos;
+    });
+  }
 
 }
