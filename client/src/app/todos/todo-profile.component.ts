085eab8224cfbc35705faac8b99878914f435065
Simon <simonhm31@gmail.com>
2024-02-14 21:39:17 -0600

    Created all todo components

---
+++ client/src/app/todos/todo-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-profile.component.html',
+  styleUrl: './todo-profile.component.scss'
+})
+export class TodoProfileComponent {
+
+}
