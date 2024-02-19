11c989fb2a8a86057f28fdcb9d8bd1dc54e0963a
NateFerrell <ferre272@umn.edu>
2024-02-19 01:25:07 -0600

    Hugge boost to test coverage ~88% from 70%, plus fixes for routing,and fixed todo add feature set.

--- client/src/app/todos/todo-profile.component.ts
+++ client/src/app/todos/todo-profile.component.ts
@@ -35,9 +35,9 @@ export class TodoProfileComponent implements OnInit, OnDestroy {
     // The map step takes the `ParamMap` from the `ActivatedRoute`, which
     // is typically the URL in the browser bar.
     // The result from the map step is the `id` string for the requested
-    // `User`.
+    // `Todo`.
     // That ID string gets passed (by `pipe`) to `switchMap`, which transforms
-    // it into an Observable<User>, i.e., all the (zero or one) `User`s
+    // it into an Observable<Todo>, i.e., all the (zero or one) `Todo`s
     // that have that ID.
     // The `takeUntil` operator allows this pipeline to continue to emit values
     // until `this.ngUnsubscribe` emits a value, saying to shut the pipeline
@@ -45,9 +45,9 @@ export class TodoProfileComponent implements OnInit, OnDestroy {
     this.route.paramMap.pipe(
       // Map the paramMap into the id
       map((paramMap: ParamMap) => paramMap.get('id')),
-      // Maps the `id` string into the Observable<User>,
+      // Maps the `id` string into the Observable<Todo>,
       // which will emit zero or one values depending on whether there is a
-      // `User` with that ID.
+      // `Todo` with that ID.
       switchMap((id: string) => this.todoService.getTodoById(id)),
       // Allow the pipeline to continue to emit values until `this.ngUnsubscribe`
       // returns a value, which only happens when this component is destroyed.
@@ -72,7 +72,7 @@ export class TodoProfileComponent implements OnInit, OnDestroy {
        * We removed it since we were not doing anything interesting on completion
        * and didn't want to clutter the console log
        */
-      // complete: () => console.log('We got a new user, and we are done!'),
+      // complete: () => console.log('We got a new todo, and we are done!'),
     });
   }
 
